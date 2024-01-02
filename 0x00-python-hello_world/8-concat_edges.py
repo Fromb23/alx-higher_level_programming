c7fb3dbed44e22a6e72b6dec8b5eabcc5de7adb8
Fromb23 <rombo.programming@gmail.com>
2024-01-02 01:32:30 -0800

    correction of task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str1 = " with Python"
-print(str[39:66] + str1)
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str2 = " with Python"
+print(' '.join(str[str.find("object-oriented programming"):].split(maxsplit=2)[:2]) + str2, end="\n")
