6521351ec78736c2268bdcfc8417ca09cec769be
Fromb23 <rombo.programming@gmail.com>
2024-01-23 13:22:23 -0800

    printing a square with #

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -49,8 +49,6 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        if value == 0:
-            print()
         self._size = value
 
     def area(self):
@@ -66,6 +64,8 @@ class Square:
         """
         Prints the square using '#' characters.
         """
+        if self._size == 0:
+            print()
         for i in range(self._size):
             for j in range(self._size):
                 print("#", end="")
