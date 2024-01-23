d2d0bdf3e07eb1459249cd3403f20f7de22115a6
Fromb23 <rombo.programming@gmail.com>
2024-01-23 10:03:31 -0800

    an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -12,25 +12,4 @@ class Square:
     Attributes:
     - length (int): The length of the square's sides.
     """
-    def __init__(self, length=None):
-        """
-        Initializes a new Square instance.
-
-        Parameters:
-        - length (int, optional): The length of the
-        square's sides. Defaults to None.
-        """
-        self.length = length
-
-    def area(self):
-        """
-        Calculates the area of the square.
-
-        Returns:
-        - int: The area of the square.
-
-        Raises:
-        - ValueError: If the length is not provided
-        or is not a positive number.
-        """
-        return self.length * self.length
+    pass
