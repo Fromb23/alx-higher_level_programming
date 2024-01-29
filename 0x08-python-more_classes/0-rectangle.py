19c852a97a281693850dc127b5353455dfebc037
Fromb23 <rombo.programming@gmail.com>
2024-01-29 11:05:42 -0800

    an empty class Rectangle that defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
 class Rectangle:
-    """
-    This is a simple Rectangle class.
-
-    Attributes:
-    - width (int): Width of the rectangle.
-    - height (int): Height of the rectangle.
-    """
     pass
