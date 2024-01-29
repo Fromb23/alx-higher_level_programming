8a3a489dd19a9e80a4fbcb5d25ea7e2c17ce0019
Fromb23 <rombo.programming@gmail.com>
2024-01-29 13:35:19 -0800

    getting the area and perimeter of class Rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+This module defines a simple Rectangle class.
+
+Examples:
+    >>> Rectangle = __import__('0-rectangle').Rectangle
+
+    >>> my_rectangle = Rectangle()
+    >>> print(type(my_rectangle))
+    <class '0-rectangle.Rectangle'>
+
+    >>> print(my_rectangle.__dict__)
+    {}
+"""
+
+
 class Rectangle:
     """
     Rectangle class represents a rectangle with width and height.
