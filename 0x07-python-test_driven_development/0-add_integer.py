80060f2c0c0c5de9565974a087a331619ede1bb4
Fromb23 <rombo.programming@gmail.com>
2024-02-01 14:58:48 -0800

    modified the docstring of task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-"""This module provides a function to add two
-    integers or floats."""
+"""
+This module provides a
+    function to add two
+    integers or floats.
+"""
 
 
 def add_integer(a, b=98):
     """
-    Adds two integers or floats and returns the result.
-
-    Parameters:
-    - a (int or float): The first number.
-    - b (int or float, optional): The second number. Default is 98.
-
-    Returns:
-    int: The sum of the two numbers, converted to an integer.
-
-    Raises:
-    TypeError: If either 'a' or 'b' is not an integer or float.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
