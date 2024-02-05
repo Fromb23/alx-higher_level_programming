5d398b3e1f28bce560a35ee9d940523e23de8ad0
Fromb23 <rombo.programming@gmail.com>
2024-02-05 15:13:53 -0800

    a class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
-"""
-Module: my_int
-
-Description:
-    MyInt class inherits from int with inverted equality operators.
-"""
-
-
 class MyInt(int):
+    """
+    MyInt class inherits from int with inverted equality operators.
+
+    Methods:
+        __eq__(self, other): Inverts the == operator.
+        __ne__(self, other): Inverts the != operator.
+    """
     def __eq__(self, other):
-        """Inverts the == operator."""
-        return super().__ne__(other)
+        """
+        Inverts the == operator.
+
+        Args:
+            other: The object to compare against.
+
+        Returns:
+            bool: True if not equal, False if equal.
+        """
+        if isinstance(other, MyInt):
+            return not super().__eq__(other)
+        else:
+            return False
 
     def __ne__(self, other):
-        """Inverts the != operator."""
-        return super().__eq__(other)
+        """
+        Inverts the != operator.
+
+        Args:
+            other: The object to compare against.
+
+        Returns:
+            bool: True if not equal, False if equal.
+        """
+        if isinstance(other, MyInt):
+            return not super().__ne__(other)
+        else:
+            return False
