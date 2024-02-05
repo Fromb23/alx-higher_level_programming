99d5d7b8c451d06ac9ada643fc276166ede06111
Fromb23 <rombo.programming@gmail.com>
2024-02-05 13:09:21 -0800

    BaseClass has now def area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Class: BaseGeometry - Represents the base geometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Placeholder for the base geometry class.
+
+    Methods:
+        area(self) - Raises an exception indicating that
+        area() is not implemented.
+    """
+    def area(self):
+        """
+        Raises an exception indicating that area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
