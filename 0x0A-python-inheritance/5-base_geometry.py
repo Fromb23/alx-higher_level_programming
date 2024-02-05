9cfad73b7a0719d6b377e4fdc93b8c2f32c26595
Fromb23 <rombo.programming@gmail.com>
2024-02-05 12:47:37 -0800

    an empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class: BaseGeometry - Represents the base geometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Placeholder for the base geometry class.
+    """
+    pass
