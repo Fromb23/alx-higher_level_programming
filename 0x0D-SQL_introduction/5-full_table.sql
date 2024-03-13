cbfafc48f85f0206fd67496d5f7a26c4759eeddc
Fromb23 <rombo.programming@gmail.com>
2024-03-13 00:26:35 -0700

    a script that prints the full description of the table first_table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- a script that print full description of a table
 -- from a database if it exists
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = first_table AND TABLE_SCHEMA = mysql
+SHOW CREATE first_table;
