aa7a450e6ece0f8aa214c7ada7d80d3b3ecfa59a
Fromb23 <rombo.programming@gmail.com>
2024-03-13 00:04:59 -0700

    firtst table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Check if the table already exists
 -- If it does, exit
-CREATE TABLE IF NOT EXISTS first_table('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table('id' INT, 'name' VARCHAR(255));
