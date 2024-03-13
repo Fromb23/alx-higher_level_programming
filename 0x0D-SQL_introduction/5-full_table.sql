b2fe1022ab6d4127ef313235b69bfc338b3a474b
Fromb23 <rombo.programming@gmail.com>
2024-03-13 00:22:05 -0700

    a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- from a database if it exists
 SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'your_table_name' AND TABLE_SCHEMA = 'your_database_name';
+WHERE TABLE_NAME = first_table AND TABLE_SCHEMA = mysql
