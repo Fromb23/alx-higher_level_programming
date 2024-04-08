2aa4276dee9d04442382e948f60388049089d83e
Fromb23 <rombo.programming@gmail.com>
2024-04-08 22:11:05 +0300

    js loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const array = [];
+
+array[0] = 'C is fun';
+array[1] = 'Python is cool';
+array[2] = 'JavaScript is amazing';
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
