eb4c1b083ccfeb5161f2eaca398e45454a075a18
Fromb23 <rombo.programming@gmail.com>
2024-04-09 00:24:01 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,15 @@
 
 function factorial (n) {
   if (n <= 1) {
-    return 1;
+    return n;
   } else {
     return n * factorial(n - 1);
   }
 }
 
-const arg = parseInt(process.argv[2]);
+let arg = 1;
+if (process.argv[2] !== undefined) {
+  arg = parseInt(process.argv[2]);
+}
 
 console.log(factorial(arg));
