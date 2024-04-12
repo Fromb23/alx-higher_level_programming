315a584f444c010490746397c32863ed28468918
Fromb23 <rombo.programming@gmail.com>
2024-04-13 00:41:46 +0300

    a python file that contains the class definition of a State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,36 +3,25 @@
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-
+# Create Base instance
 Base = declarative_base()
 
 
 class State(Base):
-
     """
     Represents a state in the database
     """
     __tablename__ = 'states'
+
+    # Define columns
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
 
-# create engine
-engine = create_engine('mysql://root:root@localhost:3306/hbtn_0e_6_usa')
+# Create engine
+engine = create_engine(('mysql+mysqldb://root:root '
+                        '@localhost:3306/hbtn_0e_6_usa'))
 
 # Create all tables
 Base.metadata.create_all(engine)
-
-# Create session
-Session = sessionmaker(bind=engine)
-
-# Create a session obj
-session = Session()
-
-# Query all db using session
-state = session.query(State).all()
-
-# Close session
-session.close()
