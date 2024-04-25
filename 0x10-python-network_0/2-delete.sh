3369f8038b0eb969d85ba897f3f771662d969288
Fromb23 <rombo.programming@gmail.com>
2024-04-25 21:52:35 +0300

    delete a request

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Check if URL is provided as an arg
-if [ $# -eq 0 ];
-then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
 # Use curl with -X opt and DELETE method to send a request to the provided URL
 curl -sX DELETE "$1"
