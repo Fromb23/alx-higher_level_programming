be97d829ab49cbf16f161f1b5121063867723a58
Fromb23 <rombo.programming@gmail.com>
2024-04-26 21:03:22 +0300

    sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,14 @@
 
 """Fetches the body of a URL using urllib."""
 
-import urllib.request
+if __name__ == '__main__':
+    import urllib.request
 
-URL = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(URL) as response:
-    read = response.read()
-    string_bytes = read.decode('utf-8')
-    print("- Body response: ")
-    print("\t- type: ", type(read))
-    print("\t- content:", read)
-    print("\t- utf8 content:", string_bytes)
+    URL = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(URL) as response:
+        read = response.read()
+        string_bytes = read.decode('utf-8')
+        print("- Body response: ")
+        print("\t- type: ", type(read))
+        print("\t- content:", read)
+        print("\t- utf8 content:", string_bytes)
