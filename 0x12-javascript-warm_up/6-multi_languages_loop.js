204ff0b32fbdb2e0b3171c0b7483a40534645e91
Fromb23 <rombo.programming@gmail.com>
2024-06-17 18:35:19 +0300

    loops in node

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const value = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const value = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < value.length; i++) {
   console.log(value[i]);
