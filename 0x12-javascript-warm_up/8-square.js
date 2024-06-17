e549275e4b112005799f4df6f1a6e28b43a05c89
Fromb23 <rombo.programming@gmail.com>
2024-06-17 19:09:06 +0300

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 
-if (process.argv < 2 || isNaN(process.argv[2])) {
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  const size = process.argv[2];
+  let square = '';
   for (let i = 0; i < size; i++) {
-    let row = ' ';
+    let row = '';
     for (let j = 0; j < size; j++) {
       row += 'X';
     }
-    console.log(row);
+    square += row + '\n';
   }
+  console.log(square.trim());
 }
