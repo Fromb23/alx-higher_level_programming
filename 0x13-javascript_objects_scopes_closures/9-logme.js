ba3a0ae26239814c435a8d89f5fa331432ff81bf
Fromb23 <rombo.programming@gmail.com>
2024-06-18 19:23:42 +0300

    logme

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -4,10 +4,10 @@
 let count = 0;
 
 // Define the function to export
-exports.logMe = function(item) {
-    // Print the current count and the item value in the specified format
-    console.log(`${count}: ${item}`);
+exports.logMe = function (item) {
+  // Print the current count and the item value in the specified format
+  console.log(`${count}: ${item}`);
 
-    // Increment the count for the next call
-    count++;
+  // Increment the count for the next call
+  count++;
 };
