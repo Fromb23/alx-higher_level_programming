dccc5f498852dc8e0f198290ec709f42437a824f
Fromb23 <rombo.programming@gmail.com>
2024-06-18 17:36:09 +0300

    more methods in the class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof h === 'number' && typeof w === 'number' && h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const neutral = this.height;
+    this.height = this.width;
+    this.width = neutral;
+  }
+
+  double () {
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+module.exports = Rectangle;
