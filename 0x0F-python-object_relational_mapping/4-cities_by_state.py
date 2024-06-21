556eb4e5bc791cab8fb1bdb2298322344e36230a
Fromb23 <rombo.programming@gmail.com>
2024-06-21 09:58:47 +0300

    select cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL database and
+retrieves all rows from the 'cities' table,ordering
+the results by 'id' in ascending order.
+"""
 import MySQLdb
 import sys
 
+
 def list_by_state(username, password, db_name):
+    """
+    Connects to the database and retrieves all cities,
+    ordered by 'id' in ascending order.
+    """
     db = MySQLdb.connect(
             user=username,
             passwd=password,
