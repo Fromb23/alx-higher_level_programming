302d7887b2903710d5461054ee98e22675e80c64
Fromb23 <rombo.programming@gmail.com>
2024-06-21 14:00:13 +0300

    first state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This script defines a State class and creates an
+instance of Base using SQLAlchemy.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Instance of the Base class
+Base = declrative_base()
+
+
+class State(Base):
+    """
+    State class that links to the MySQL table 'states'.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
