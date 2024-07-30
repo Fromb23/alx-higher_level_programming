edf2f8c3dbe4c3f92ab5105d53e464f61310b35f
Fromb23 <rombo.programming@gmail.com>
2024-07-30 15:08:59 +0300

    statusCode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,8 +5,8 @@ const url = process.argv[2];
 
 request(url, (err, response, body) => {
   if (err) {
-    console.error(err);
+    console.error('Error:', err);
     return;
   }
-  console.log('Code:', response.statusCode);
+  console.log('code:', response.statusCode);
 });
