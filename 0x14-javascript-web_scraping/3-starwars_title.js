8f9aa8c0d8b2bef6cd6a7cf1478014c64e69c67a
Fromb23 <rombo.programming@gmail.com>
2024-07-30 15:24:04 +0300

    star wars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+
+  if (response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  } else {
+    console.log(`Error: Received status code ${response.statusCode}`);
+  }
+});
